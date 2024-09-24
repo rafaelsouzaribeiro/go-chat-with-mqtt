16d5e18706d092ec6d21e1499a8134f28b2a8ef1
Rafael <rafaelribeirosouza86@gmail.com>
2024-09-24 10:59:23 -0300

    updating project

--- internal/infra/database/cassandra/repository/save-message.go
+++ internal/infra/database/cassandra/repository/save-message.go
@@ -8,14 +8,14 @@ import (
 	"github.com/rafaelsouzaribeiro/go-chat-with-mqtt/internal/entity"
 )
 
-func (i *CassandraRepository) SaveMessage(input *entity.Message) (*entity.Message, error) {
+func (i *CassandraRepository) PublishMessage(input *entity.Message) error {
 
 	insert := fmt.Sprintf("INSERT INTO %s.messages(userid,message,username,pages,times)VALUES(?,?,?,?)", entity.KeySpace)
 	err := i.gocql.Query(insert, uuid.NewString(), input.Message, input.Username, 1, time.Now()).Exec()
 
 	if err != nil {
-		return nil, err
+		return err
 	}
 
-	return input, nil
+	return nil
 }
---
+++ internal/infra/di/new-usecase.go
@@ -0,0 +1,12 @@
+package di
+
+import (
+	"github.com/gocql/gocql"
+	"github.com/rafaelsouzaribeiro/go-chat-with-mqtt/internal/infra/database/cassandra/repository"
+	"github.com/rafaelsouzaribeiro/go-chat-with-mqtt/internal/usecase"
+)
+
+func NewUseCase(db *gocql.Session) *usecase.UseCaseMessageUser {
+	repository := repository.NewCassandraRepository(db)
+	return usecase.NewUsecase(repository)
+}
