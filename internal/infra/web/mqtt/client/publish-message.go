8549b32278fde07cd0540642718ed780f2df35f9
Rafael <rafaelribeirosouza86@gmail.com>
2024-09-25 12:01:20 -0300

    updating client

--- internal/infra/web/mqtt/client/publish-message.go
+++ internal/infra/web/mqtt/client/publish-message.go
@@ -1,10 +1,20 @@
 package client
 
 import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
 	"github.com/rafaelsouzaribeiro/go-chat-with-mqtt/internal/usecase/dto"
 )
 
-func (b *MqttClient) PublishMessage(dto *dto.Payload) error {
+func (b *MqttClient) PublishMessage(c *gin.Context) {
+	var PublishDto dto.Payload
+
+	if err := c.ShouldBindJSON(&PublishDto); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"error": err.Error()})
+		return
+	}
 
 	_, err := b.Usecase.SaveMessage(dto)
 
