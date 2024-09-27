d08ad19b736bc9ad1d25dc0c1f36df08ef6671aa
Rafael <rafaelribeirosouza86@gmail.com>
2024-09-27 12:03:17 -0300

    update received messages

--- internal/infra/web/mqtt/server/index.go
+++ internal/infra/web/mqtt/server/index.go
@@ -9,6 +9,7 @@ import (
 	mqtt "github.com/mochi-mqtt/server/v2"
 	"github.com/mochi-mqtt/server/v2/hooks/auth"
 	"github.com/mochi-mqtt/server/v2/listeners"
+	"github.com/mochi-mqtt/server/v2/packets"
 )
 
 func (b *Broker) StartServer() {
@@ -38,6 +39,11 @@ func (b *Broker) StartServer() {
 	if err != nil {
 		panic(err)
 	}
+
+	callbackFn := func(cl *mqtt.Client, sub packets.Subscription, pk packets.Packet) {
+		fmt.Println("inline client received message from subscription", "client", cl.ID, "subscriptionId", sub.Identifier, "topic", pk.TopicName, "payload", string(pk.Payload))
+	}
+	server.Subscribe("topic/test", 1, callbackFn)
 
 	go func() {
 		err := server.Serve()
