faf7c2b70c990c11a4e0e96fdc6640a7c4e9d764
Rafael <rafaelribeirosouza86@gmail.com>
2024-10-08 02:52:05 -0300

    adding auth

--- internal/infra/web/handler/lists.go
+++ internal/infra/web/handler/lists.go
@@ -7,6 +7,25 @@ import (
 )
 
 func (o *ChatHandler) Lists(c *gin.Context) {
+	session, err := store.Get(c.Request, "go-chat")
+
+	if err != nil {
+		o.ClearSession(c, "go-chat")
+	}
+
+	result := session.Flashes()
+
+	if result == nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "value not set"})
+		return
+	}
+
+	_, err = o.chatUseCase.CheckUser(result[1].(string), result[0].(string))
+
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "user not logged in"})
+		return
+	}
 
 	output, err := o.chatUseCase.ListUsers()
 
